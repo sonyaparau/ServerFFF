aff758b9a9caf82e57b9b58c8b13d21bb1e4e0f5
Margareta Puscas <marmarpuscas@gmail.com>
2019-12-04 21:01:53 +0200

    filter locals after type

--- business/src/main/java/com/mobile/freeforfun/business/service/LocalService.java
+++ business/src/main/java/com/mobile/freeforfun/business/service/LocalService.java
@@ -1,9 +1,12 @@
 package com.mobile.freeforfun.business.service;
 
 import com.mobile.freeforfun.business.dto.LocalDto;
+import com.mobile.freeforfun.persistence.enums.ELocalType;
+
 import java.util.List;
 
 public interface LocalService {
 
 	List<LocalDto> getAllLocals();
+	List<LocalDto> filterLocalAfterType(ELocalType localType);
 }
